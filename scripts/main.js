d7c6ea09812a8dd86402f14462d767b4bfbc4f68
Bartosz Stec <bartoszstec7@gmail.com>
2025-07-27 00:59:53 +0200

    Extended game logic
    added listed game functions: checkIfDraw, resetGame, results. Event listener modificated. Structure of main.js slightly reorganized.

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,7 @@
             this.width = width;
             this.height = height;
             this.cellSize = this.width/3;
+            this.gameOver = false;
             this.board = [
             [null, null, null], //tablica gry
             [null, null, null],
@@ -29,26 +30,34 @@
         //check row
         for (let row = 0; row < 3; row++ ){
             if (b[row][0] && b[row][0] === b[row][1] && b[row][1] === b[row][2]) {
-                console.log("koniec gry wygrał:", this.currentPlayer);
+                //console.log("koniec gry wygrał:", this.currentPlayer);
+                return true;
             }
         }
 
         //check col
         for (let col = 0; col < 3; col++ ){
             if (b[0][col] && b[0][col] === b[1][col] && b[1][col] === b[2][col]) {
-                console.log("koniec gry wygrał:", this.currentPlayer);
+                //console.log("koniec gry wygrał:", this.currentPlayer);
+                return true;
             }
         }
 
         //check diagonal from [0][0]
             if (b[0][0] && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
-                console.log("koniec gry wygrał:", this.currentPlayer);
+                //console.log("koniec gry wygrał:", this.currentPlayer);
+                return true;
             }
 
         //check diagonal from [0][3]
             if (b[0][2] && b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
-                    console.log("koniec gry wygrał:", this.currentPlayer);
+                //console.log("koniec gry wygrał:", this.currentPlayer);
+                return true;
             }
+        }
+
+        checkIfDraw() {
+        return this.board.flat().every(cell => cell !== null);
         }
 
         drawBoard(ctx) {
@@ -76,6 +85,31 @@
         ctx.moveTo(0, this.cellSize * 2);
         ctx.lineTo(this.width, this.cellSize * 2);
         ctx.stroke();
+        }
+
+        resetGame(){
+        this.board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+        ];
+
+        this.currentPlayer = "X";
+        ctx.clearRect(0, 0, this.width, this.height);
+        this.drawBoard(ctx);
+        this.gameOver = false;
+        }
+
+        results(winner){
+            this.gameOver = true;
+            if (winner){
+                console.log("koniec gry wygrał:", game.currentPlayer);
+            } else {console.log("remis");}
+
+            console.log("tutaj jest czas na animację");
+            setTimeout(() => {
+            game.resetGame(); 
+            }, 1000);
         }
 
         drawSymbol(ctx, row, col, symbol) {
@@ -108,23 +142,29 @@
     const game = new Game(canvas.width, canvas.height);
     game.drawBoard(ctx);
 
+
+    //nasłuchiwacz kliknięć
     canvas.addEventListener("click", function(event) {
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+        if(!game.gameOver) {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
 
-    const col = Math.floor(x / game.cellSize);
-    const row = Math.floor(y / game.cellSize);
+        const col = Math.floor(x / game.cellSize);
+        const row = Math.floor(y / game.cellSize);
 
-    console.log("Kliknięto:", { row, col, player: game.currentPlayer });
+        console.log("Kliknięto:", { row, col, player: game.currentPlayer });
 
-    // Warunek sprawdzający czy pole jest puste i wstawiający odpowiedni symbol
-    if (game.board[row][col] === null) {
-        game.board[row][col] = game.currentPlayer;
-        game.drawSymbol(ctx, row, col, game.currentPlayer);
-        game.checkIfWin();
-        game.switchPlayer();
-    }
+            // Warunek sprawdzający czy pole jest puste i wstawiający odpowiedni symbol
+            if (game.board[row][col] === null) {
+                game.board[row][col] = game.currentPlayer;
+                game.drawSymbol(ctx, row, col, game.currentPlayer);
+                if (game.checkIfWin()){
+                    game.results(true);
+                } else {game.switchPlayer();}
+            }
 
-});
+            if (game.checkIfDraw()){game.results(false);}
+        }
+    });
 
